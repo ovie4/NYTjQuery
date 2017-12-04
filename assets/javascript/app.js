e965b7bc40e957e982c69f738b564e245bc597c7
Ovie <you@example.com>
2017-12-04 10:42:51 -0500

    adds click event handler

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
-
+	var newSearchValue;
 	//on click
+	$(".btn").on("click", function(){
+		event.preventDefault()
+		
+		 newSearchValue = $("#search-input").val().trim();
+
+	})
 	//capture input value
 	//get ajax pull
+	//display on screen
 })
