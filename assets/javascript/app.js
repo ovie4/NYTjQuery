242fbd615aaf8a950b7d76f461601f879274e2ce
Mubin Akhawala <m.akhawala@yahoo.com>
2017-12-04 10:42:50 -0500

    ajax api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
 
-	//on click
-	//capture input value
-	//get ajax pull
+	//variables for search items
+	var searchTerm = "";
+	var results = 0;
+
+	var QueryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "5af7fa04ca564c049d7e43ef68469a48"
+});
+
+
+$.ajax({
+  url: queryUrl,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
 })
