0fe26d85652f271dd99d4580c57ac87e07a5c9d7
Ovie <you@example.com>
2017-12-04 10:52:41 -0500

    adds display on page function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
 
-	var newSearchValue;
+	var results;
 	//on click
 	$(".btn").on("click", function(){
 		event.preventDefault()
 		
-		 newSearchValue = $("#search-input").val().trim();
+		 results = $("#search-input").val().trim();
 
 	})
 	//capture input value
 	//get ajax pull
-	//display on screen
+	
 
 
 	//variables for search items
 	var searchTerm = "";
 	var results = 0;
 
-	var QueryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+	var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 url += '?' + $.param({
   'api-key': "5af7fa04ca564c049d7e43ef68469a48"
 });
@@ -33,3 +33,14 @@ $.ajax({
 });
 
 })
+
+//display on screen
+function displayResults(){
+	var resultsDiv = $("<div>");
+	for (var i = 0; i < ajax_data.length; i++) {
+		var newArticle = ajax_data[i].headline;
+		resultsDiv.append(newArticle);
+		$("#search-results").append(resultsDiv);
+	}
+}
+
